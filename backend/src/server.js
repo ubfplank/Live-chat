c904c82d869fa364598657b83f8ca1cca06b6e8f
Ubfplank <ghopb1108@gmail.com>
2025-08-28 18:04:23 -0300

    j

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,21 @@
-const { WebSocketServer} = require ("ws"
-const dotenv = require ("dotenv")
+const { WebSocketServer } = require("ws");
+const dotenv = require("dotenv");
 
-dotenv.config()
-    const wss = new WebSocketServer ({ port: process.env.PORT||8080})
+dotenv.config();
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 
 wss.on("connection", (ws) => {
-    ws.on("error",console.error)
+    ws.on("error", console.error);
 
-    ws.on("message," (data) => {
-       
-        ws.on ("message",(data) => {
-            wss.clients.forEach((client) => client.send (data.toString()))
-        }
+    ws.on("message", (data) => {
+        // Envia a mensagem recebida para todos os clientes conectados
+        wss.clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+                client.send(data.toString());
+            }
+        });
+    });
 
-    }) 
-
-    console.log ("client connected")
-})
+    console.log("Client connected");
+});
